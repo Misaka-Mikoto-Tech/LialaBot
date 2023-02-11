9cb4cef41d7ddee630711f01cdca6b8b19f886ba
Misaka Mikoto <easy66@live.com>
2023-02-12 04:09:04 +0800

    * 修正本地源码运行hb时找不到version的问题

--- haruka_bot/version.py
+++ haruka_bot/version.py
@@ -2,5 +2,8 @@ from importlib.metadata import version
 
 from packaging.version import Version
 
-__version__ = version("haruka-bot")
+try:
+    __version__ = version("haruka-bot")
+except Exception:
+    __version__='dev'
 VERSION = Version(__version__)
