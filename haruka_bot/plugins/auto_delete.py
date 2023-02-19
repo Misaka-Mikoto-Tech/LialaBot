c2adf80569cb19dc22d80f2510e2fd0178dc037e
Misaka Mikoto <easy66@live.com>
2023-02-20 07:47:57 +0800

    * 修改退群发送api

--- haruka_bot/plugins/auto_delete.py
+++ haruka_bot/plugins/auto_delete.py
@@ -21,13 +21,14 @@ async def _(event: Union[GroupDecreaseNoticeEvent, ChannelDestroyedNoticeEvent],
             await db.delete_group(id=event.group_id)
         else:
             logger.warning(f"bot:{bot.self_id}: {event.user_id}退出群{event.group_id}")
-            await safe_send(
-                bot_id=bot.self_id,
-                send_type='group',
-                type_id=event.group_id,
-                message="呜, 有人跑了",
-                at=False,
-            )
+            # await safe_send(
+            #     bot_id=bot.self_id,
+            #     send_type='group',
+            #     type_id=event.group_id,
+            #     message="呜, 有人跑了",
+            #     at=False,
+            # )
+            await group_decrease.finish("呜, 有人跑了")
     elif isinstance(event, ChannelDestroyedNoticeEvent):
         await db.delete_sub_list(type="guild", type_id=await get_type_id(event), bot_id=bot.self_id)
         await db.delete_guild(id=await get_type_id(event))
