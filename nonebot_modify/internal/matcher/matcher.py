8e71d2a53cf09feca9f451fbd2df051c128e3cc3
Misaka Mikoto <easy66@live.com>
2023-06-19 22:44:29 +0800

    + 2.0.0 正式版原始文件

--- nonebot_modify/internal/matcher/matcher.py
+++ nonebot_modify/internal/matcher/matcher.py
@@ -738,7 +738,7 @@ class Matcher(metaclass=MatcherMeta):
             except StopPropagation:
                 self.block = True
             finally:
-                logger.debug(f"{self} running complete")
+                logger.info(f"{self} running complete")
 
     # 运行handlers
     async def run(
