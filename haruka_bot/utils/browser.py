0fe6a4ab289afbd0dc94c86de796e6bd0b92732e
Misaka Mikoto <easy66@live.com>
2023-07-20 17:19:00 +0800

    * 动态截图自动填cookie

--- haruka_bot/utils/browser.py
+++ haruka_bot/utils/browser.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 import re
 import sys
+import json
 from pathlib import Path
 from typing import Optional
 
@@ -50,15 +51,17 @@ async def get_dynamic_screenshot(dynamic_id, style=config.haruka_screenshot_styl
 async def get_dynamic_screenshot_mobile(dynamic_id):
     """移动端动态截图"""
     url = f"https://m.bilibili.com/dynamic/{dynamic_id}"
+    cookies = json.loads(Path('./bili_cookies.json').read_text("utf-8"))  # type: ignore
     browser:Browser = await get_browser()
     page = await browser.new_page(
         device_scale_factor=2,
         user_agent=(
-            "Mozilla/5.0 (Linux; Android 11; RMX3161 Build/RKQ1.201217.002; wv) AppleWebKit/537.36 "
-            "(KHTML, like Gecko) Version/4.0 Chrome/101.0.4896.58 Mobile Safari/537.36"
+            "Mozilla/5.0 (Linux; Android 11; RMX3161 Build/RKQ1.201217.003; wv) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Version/4.0 Chrome/101.0.4896.59 Mobile Safari/537.36"
         ),
         viewport={"width": 460, "height": 780},
     )
+    await page.context.add_cookies(cookies)
     try:
         await page.route(re.compile("^https://static.graiax/fonts/(.+)$"), fill_font)
         await page.goto(
